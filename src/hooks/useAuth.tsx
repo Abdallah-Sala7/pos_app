147c6c50c0ae7487e5d4338c6617f3c8379f173e
Abdallah-sala7 <Abdallah-Sala7@users.noreply.github.com>
2025-04-28 03:52:00 +0300

    Update User Auth

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,51 +1,85 @@
 import { saveUserInfo, storeToken } from "@/store/reducer/auth";
+import { useGetUserInfoQuery } from "@/store/server/userApi";
 import { authenticationRoutes } from "@/helper/routes";
 
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { RouteObject, useLocation, useNavigate } from "react-router-dom";
-import { setProducts } from "@/store/reducer/appSlice";
 
-function useAuth() {
+function useAuth(): boolean {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
   const { pathname } = useLocation();
 
+  const dispatch = useDispatch();
+
   const stored_token = localStorage.getItem("@token");
-  const user = localStorage.getItem("@user");
-  const products = localStorage.getItem("@products");
+  const { data, isError, isLoading, isSuccess } =
+    useGetUserInfoQuery(undefined);
 
-  useEffect(() => {
-    const handleAuth = async () => {
-      if (stored_token) {
-        if (
-          authenticationRoutes.some((route: RouteObject) =>
-            [route.path].includes(pathname)
-          )
-        ) {
-          return navigate("/", {
-            replace: true,
-          });
-        }
+  const active_branch = localStorage.getItem("@active_branch");
 
-        navigate(pathname, {
+  const handleAuth = async () => {
+    if (isSuccess) {
+      if (data?.result.profile?.user_type === "cashier") {
+        localStorage.setItem(
+          "@active_branch",
+          data?.result.profile?.pos_device?.store?.id
+        );
+
+        dispatch(
+          saveUserInfo({
+            ...data?.result.profile,
+            ...data?.result,
+            permissions: data?.result.profile?.pos_device?.permissions,
+            active_branch: data?.result.profile?.pos_device?.store?.id,
+          })
+        );
+      } else {
+        dispatch(
+          saveUserInfo({
+            ...data?.result.profile,
+            ...data?.result,
+            active_branch,
+          })
+        );
+      }
+
+      if (
+        authenticationRoutes.some((route: RouteObject) =>
+          [route.path].includes(pathname)
+        )
+      ) {
+        navigate("/", {
           replace: true,
         });
-      } else {
+        return;
+      }
+
+      navigate(pathname, {
+        replace: true,
+      });
+    } else if (isError) {
+      localStorage.removeItem("@token");
+      localStorage.removeItem("@active_branch");
+      if (
+        !authenticationRoutes.some((route: RouteObject) =>
+          [route.path].includes(pathname)
+        )
+      ) {
         navigate("/login", {
           replace: true,
         });
       }
+    }
 
-      dispatch(storeToken(stored_token));
-      dispatch(saveUserInfo(user));
-      dispatch(setProducts(JSON.parse(products || "[]")));
-    };
+    dispatch(storeToken(stored_token));
+  };
 
+  useEffect(() => {
     handleAuth();
-  }, [stored_token]);
+  }, [isSuccess, isError, data]);
 
-  return null;
+  return isLoading;
 }
 
 export default useAuth;
